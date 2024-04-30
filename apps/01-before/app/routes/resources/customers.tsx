6888aa52ed1b9a3a8970c8735d5b8815c5e394fb
Saurabh Prakash <saurabhp75@gmail.com>
2024-04-30 11:04:13 +0530

    Connect loader with db

--- apps/01-before/app/routes/resources/customers.tsx
+++ apps/01-before/app/routes/resources/customers.tsx
@@ -1,7 +1,15 @@
-// 🐨 finish the owl
+import type { LoaderArgs } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import invariant from 'tiny-invariant'
+import { searchCustomers } from '~/models/customer.server'
+import { requireUser } from '~/session.server'
 
-import { json } from "@remix-run/node";
-
-export async function loader() {
-	return json({ hello: 'world' })
+export async function loader({ request }: LoaderArgs) {
+	await requireUser(request)
+	const url = new URL(request.url)
+	const query = url.searchParams.get('query')
+	invariant(typeof query === 'string', 'query is required')
+	return json({
+		customers: await searchCustomers(query),
+	})
 }
